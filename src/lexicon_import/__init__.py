362f401ad900dee415e33ac9aef4b1c4096356c1
emilyhunt <emily.hunt.physics@gmail.com>
2026-03-26 14:17:23 -0700

    Remove most GCN autoimport stuff and replace with a lil cheeky string schema

--- src/lexicon_import/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-WORKING_DIRECTORY = Path(__file__).parent.parent.parent / ".temp"
-WORKING_DIRECTORY.mkdir(exist_ok=True, parents=False)
-
-LEXICON_DIRECTORY = WORKING_DIRECTORY.parent / "lexicons"
